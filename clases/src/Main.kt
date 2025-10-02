781590e7fcfe30ee6e17ea8a1fd7d88d5dcd9e79
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-10-02 13:26:58 -0600

    calculateIMC

--- clases/src/Main.kt
+++ clases/src/Main.kt
@@ -1,7 +1,10 @@
+import kotlin.math.pow
+
 class Person(
-    name: String, age:
-    Int, dni: String,
-    sex: String,
+    name: String = "",
+    age: Int = 0,
+    dni: String = "",
+    sex: String = "M",
     weight: Double,
     height: Double
 ) {
@@ -11,9 +14,28 @@ class Person(
     private val sex: String = sex
     private val weight: Double = weight
     private val height: Double = height
+
+    fun calculateIMC(): Int {
+        val imc = this.weight / this.height.pow(2.0)
+        println(imc)
+
+        if(imc > 25)
+            return 1
+        else if(imc >= 20)
+            return 0
+        else
+            return -1
+    }
 }
 
 fun main() {
     val person1 = Person("Hanzeel", 21, "1234567890","M", 56.0, 1.75)
-    val person2 = Person("Dari", 20, "0987654321", "F",56.0, 1.80)
+    val person2 = Person("Dari", 20, "0987654321", "F",60.0, 1.80)
+
+    val person1Imc: Int = person1.calculateIMC()
+    val persona2Imc: Int = person2.calculateIMC()
+
+    println("IMC person1: $person1Imc")
+    println("IMC person2: $persona2Imc")
+
 }
